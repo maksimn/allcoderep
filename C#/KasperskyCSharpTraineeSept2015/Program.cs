9fcd8b7aa56d987823370c604552fe9f4578d89f
maksimn <maksim1989sovunion@yandex.ru>
2015-10-03 07:00:59 +0300

    Code every day

--- C#/KasperskyCSharpTraineeSept2015/Program.cs
+++ C#/KasperskyCSharpTraineeSept2015/Program.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 
 class Program {
     private static String GetAnagramString(String word) {
-        Char[] chars = word.ToCharArray(); 
+        Char[] chars = word.ToLowerInvariant().ToCharArray();
         Array.Sort(chars);
         return new String(chars);
     }
     private static void IndexStringFromStringReader(StringReader sr) {
         String word;
-        //var sortedByAnagramString = new SortedSet();
+        var structure = new List<KeyValuePair<String, String>>();
         while ((word = sr.ReadLine()) != null) {
-            Console.WriteLine(word + " " + GetAnagramString(word));
+            structure.Add(new KeyValuePair<String, String>(GetAnagramString(word), word));
+        }
+        foreach (var element in structure) {
+            Console.WriteLine(String.Format("{0} {1}", element.Key, element.Value));
         }
     }
     static void Main(String[] args) {
