303fec720b7a5e50a2ca936245e8a88c02367fe4
maksimn <maksim1989sovunion@yandex.ru>
2015-10-04 06:55:29 +0300

    I code

--- C#/KasperskyCSharpTraineeSept2015/Program.cs
+++ C#/KasperskyCSharpTraineeSept2015/Program.cs
@@ -2,23 +2,18 @@
 using System.IO;
 using System.Collections.Generic;
 
-class Program {
-    private static String GetAnagramString(String word) {
-        Char[] chars = word.ToLowerInvariant().ToCharArray();
-        Array.Sort(chars);
-        return new String(chars);
+class WordAnagramComparer : IComparer<KeyValuePair<String, String>> {
+    public Int32 Compare(KeyValuePair<String, String> kv1, KeyValuePair<String, String> kv2) {
+        return String.Compare(kv1.Key, kv2.Key, StringComparison.Ordinal);
     }
-    private static void IndexStringFromStringReader(StringReader sr) {
-        String word;
-        var structure = new List<KeyValuePair<String, String>>();
-        while ((word = sr.ReadLine()) != null) {
-            structure.Add(new KeyValuePair<String, String>(GetAnagramString(word), word));
-        }
-        foreach (var element in structure) {
-            Console.WriteLine(String.Format("{0} {1}", element.Key, element.Value));
-        }
-    }
-    static void Main(String[] args) {
+}
+
+interface IInitializer {
+    void Init(List<KeyValuePair<String, String>> list);
+}
+
+class StringReaderInitializer : IInitializer {
+    public void Init(List<KeyValuePair<String, String>> list) {
         String inputString = "абак\n" +
             "ток\n" +
             "вертикаль\n" +
@@ -28,6 +23,48 @@ class Program {
             "кот\n" +
             "уклон\n";
         StringReader sr = new StringReader(inputString);
-        IndexStringFromStringReader(sr);
+        String word;
+        while ((word = sr.ReadLine()) != null) {
+            list.Add(new KeyValuePair<String, String>(GetAnagramString(word), word));
+        }
+    }
+    private static String GetAnagramString(String word) {
+        Char[] chars = word.ToLowerInvariant().ToCharArray();
+        Array.Sort(chars);
+        return new String(chars);
+    }
+}
+
+class TaskSolver {
+    private List<KeyValuePair<String, String>> list;
+    public TaskSolver(IInitializer initializer) {
+        list = new List<KeyValuePair<String, String>>();
+        initializer.Init(list);
+    }
+    public void Process() {
+        list.Sort(new WordAnagramComparer());
+    }
+    public void Output(IOutputable iout) {
+        iout.Output(list);
+    }
+}
+
+interface IOutputable {
+    void Output(List<KeyValuePair<String, String>> list);
+}
+
+class ConsoleOutputExample1 : IOutputable {
+    public void Output(List<KeyValuePair<String, String>> list) {
+        foreach (var element in list) {
+            Console.WriteLine(String.Format("{0} {1}", element.Key, element.Value));
+        }
+    }
+}
+
+class Program {
+    static void Main(String[] args) {
+        TaskSolver solver = new TaskSolver(new StringReaderInitializer());
+        solver.Process();
+        solver.Output(new ConsoleOutputExample1());
     }
 }
