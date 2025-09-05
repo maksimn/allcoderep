f6e81c7bd949ffe784c68f9acffbb7c34e025666
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2025-09-05 13:33:48 +0300

    1

--- ML-Trainings-1.0/06-boosting/main.py
+++ ML-Trainings-1.0/06-boosting/main.py
@@ -5,3 +5,59 @@ from matplotlib import pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import make_regression
+from boosting import SimplifiedBoostingRegressor
+
+def overfitting_tests():
+    for _ in tqdm(range(10)):
+        X = np.random.randn(200, 10)
+        y = np.random.normal(0, 1, X.shape[0])
+        boosting_regressor = SimplifiedBoostingRegressor()    
+        boosting_regressor.fit(DecisionTreeRegressor, X, y, 100, 0.5, 10)
+        assert boosting_regressor.loss_log[-1] < 1e-6, 'Boosting should overfit with many deep trees on simple data!'
+        assert boosting_regressor.loss_log[0] > 1e-2, 'First tree loos should be not to low!'    
+    print('Overfitting tests done!')
+
+def zero_lr_tests():
+    for _ in tqdm(range(10)):
+        X = np.random.randn(200, 10)
+        y = np.random.normal(0, 1, X.shape[0])
+        boosting_regressor = SimplifiedBoostingRegressor()    
+        boosting_regressor.fit(DecisionTreeRegressor, X, y, 10, 0., 10)
+        predictions = boosting_regressor.predict(X)
+        assert all(predictions == 0), 'With zero weight model should predict constant values!'
+        assert boosting_regressor.loss_log[-1] == boosting_regressor.loss_log[0], 'With zero weight model should not learn anything new!'
+    print('Zero lr tests done!')
+
+def fitting_tests():
+    for _ in tqdm(range(10)):
+        data, targets = make_regression(1000, 10)
+        indices = np.arange(len(data))
+        np.random.shuffle(indices)
+        data_train, targets_train = data[indices[:700]], targets[indices[:700]]
+        data_val, targets_val = data[indices[700:]], targets[indices[700:]]
+
+
+        train_loss_log = []
+        val_loss_log = []
+        for depth in range(1, 25):
+            boosting_regressor = SimplifiedBoostingRegressor()    
+
+            boosting_regressor.fit(DecisionTreeRegressor, data_train, targets_train, depth, 0.2, 5)
+            predictions_train = boosting_regressor.predict(data_train)
+            predictions_val = boosting_regressor.predict(data_val)
+            train_loss_log.append(np.mean((predictions_train-targets_train)**2))
+            val_loss_log.append(np.mean((predictions_val-targets_val)**2))
+        
+        assert train_loss_log[-2] > train_loss_log[-1] and abs(train_loss_log[-2]/train_loss_log[-1]) < 2, '{}, {}'.format(train_loss_log[-2], train_loss_log[-1])
+
+def make_plot():
+    plt.plot(range(1, len(train_loss_log)+1), train_loss_log, label='train')
+    plt.plot(range(1, len(val_loss_log)+1), val_loss_log, label='val')
+    plt.xlabel('Ensemble size')
+    plt.ylabel('Error')
+    plt.legend()
+
+overfitting_tests()
+# zero_lr_tests()
+# fitting_tests()
+# make_plot()
