5118e5f7334f92a42da9b8d9cfb6d5a3edf7610e
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2025-09-13 21:36:51 +0300

    1

--- ML-Trainings-1.0/07-mnist-classification/main.py
+++ ML-Trainings-1.0/07-mnist-classification/main.py
@@ -38,20 +38,20 @@ class MyNeuralNetwork(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 50) # Input layer -> hidden layer
+        self.linearLayer = nn.Linear(784, 50) # Input layer -> hidden layer
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(50, 10) # Hidden layer -> output layer for 2 classes
+        self.classifier = nn.Linear(50, 10) # Hidden layer -> output layer for 2 classes
 
     def forward(self, x):
-        x = self.fc1(x)
+        x = self.linearLayer(x)
         x = self.relu(x)
-        x = self.fc2(x)
+        x = self.classifier(x)
         return x
 
 model = MyNeuralNetwork() # your code here
 
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+criterion = nn.CrossEntropyLoss()  
+optimizer = optim.Adam(model.parameters(), lr=0.1)
 
 # do not change the code in the block below
 # __________start of block__________
