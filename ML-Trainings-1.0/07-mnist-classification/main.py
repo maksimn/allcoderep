5bcdc649d72108f2f34f6612248f411a1170f07c
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2025-09-13 14:45:09 +0300

    MNIST Classification.

---
+++ ML-Trainings-1.0/07-mnist-classification/main.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import torchvision
+from torchvision.datasets import MNIST
+
+from matplotlib import pyplot as plt
+# from IPython.display import clear_output
+
+# do not change the code in the block below
+# __________start of block__________
+
+train_mnist_data = MNIST('.', train=True, transform=torchvision.transforms.ToTensor(), download=True)
+test_mnist_data = MNIST('.', train=False, transform=torchvision.transforms.ToTensor(), download=True)
+
+
+train_data_loader = torch.utils.data.DataLoader(
+    train_mnist_data,
+    batch_size=32,
+    shuffle=True,
+    num_workers=2
+)
+
+test_data_loader = torch.utils.data.DataLoader(
+    test_mnist_data,
+    batch_size=32,
+    shuffle=False,
+    num_workers=2
+)
+
+random_batch = next(iter(train_data_loader))
+_image, _label = random_batch[0][0], random_batch[1][0]
+plt.figure()
+plt.imshow(_image.reshape(28, 28))
+plt.title(f'Image label: {_label}')
+# __________end of block__________
+
+# Creating model instance
+model = None # your code here
+
+# do not change the code in the block below
+# __________start of block__________
+assert model is not None, 'Please, use `model` variable to store your model'
+
+try:
+    x = random_batch[0].reshape(-1, 784)
+    y = random_batch[1]
+
+    # compute outputs given inputs, both are variables
+    y_predicted = model(x)    
+except Exception as e:
+    print('Something is wrong with the model')
+    raise e
+    
+    
+assert y_predicted.shape[-1] == 10, 'Model should predict 10 logits/probas'
+
+print('Everything seems fine!')
+# __________end of block__________
+
+# your code here
+
+predicted_labels = []
+real_labels = []
+model.eval()
+with torch.no_grad():
+    for batch in train_data_loader:
+        y_predicted = model(batch[0].reshape(-1, 784))
+        predicted_labels.append(y_predicted.argmax(dim=1))
+        real_labels.append(batch[1])
+
+predicted_labels = torch.cat(predicted_labels)
+real_labels = torch.cat(real_labels)
+train_acc = (predicted_labels == real_labels).type(torch.FloatTensor).mean()
+print(f'Neural network accuracy on train set: {train_acc:3.5}')
+predicted_labels = []
+real_labels = []
+model.eval()
+with torch.no_grad():
+    for batch in test_data_loader:
+        y_predicted = model(batch[0].reshape(-1, 784))
+        predicted_labels.append(y_predicted.argmax(dim=1))
+        real_labels.append(batch[1])
+
+predicted_labels = torch.cat(predicted_labels)
+real_labels = torch.cat(real_labels)
+test_acc = (predicted_labels == real_labels).type(torch.FloatTensor).mean()
+print(f'Neural network accuracy on test set: {test_acc:3.5}')
+
+assert test_acc >= 0.92, 'Test accuracy is below 0.92 threshold'
+assert train_acc >= 0.91, 'Train accuracy is below 0.91 while test accuracy is fine. We recommend to check your model and data flow'
+
+import os
+
+assert os.path.exists('hw07_data_dict.npy'), 'Please, download `hw07_data_dict.npy` and place it in the working directory'
+
+def get_predictions(model, eval_data, step=10):
+    
+    predicted_labels = []
+    model.eval()
+    with torch.no_grad():
+        for idx in range(0, len(eval_data), step):
+            y_predicted = model(eval_data[idx:idx+step].reshape(-1, 784))
+            predicted_labels.append(y_predicted.argmax(dim=1))
+    
+    predicted_labels = torch.cat(predicted_labels)
+    return predicted_labels
+
+loaded_data_dict = np.load('hw07_data_dict.npy', allow_pickle=True)
+
+submission_dict = {
+    'train': get_predictions(model, torch.FloatTensor(loaded_data_dict.item()['train'])).numpy(),
+    'test': get_predictions(model, torch.FloatTensor(loaded_data_dict.item()['test'])).numpy()
+}
+
+np.save('submission_dict_hw07.npy', submission_dict, allow_pickle=True)
+print('File saved to `submission_dict_hw07.npy`')
+# __________end of block__________
